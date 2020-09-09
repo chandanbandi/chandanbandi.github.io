707d55635400e60b4acbc6fd352ab73da602caf3
chandanbandi <bchandu2988@gmail.com>
2020-09-09 10:44:02 +0530

    Updates

---
+++ precache-manifest.610085e3f758caf75c9475573ee3329b.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23dbb8305943891687495f3dc98500a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c3fc3cd2558e71abdec",
+    "url": "/static/css/main.10d695eb.chunk.css"
+  },
+  {
+    "revision": "f6f1108c06f866c97eb6",
+    "url": "/static/js/2.88efb32c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.88efb32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c3fc3cd2558e71abdec",
+    "url": "/static/js/main.0d966a4e.chunk.js"
+  },
+  {
+    "revision": "9b41c7e13f690904baac",
+    "url": "/static/js/runtime-main.c3e8af0c.js"
+  },
+  {
+    "revision": "4fc5c26b7b68057503e480cb953aea22",
+    "url": "/static/media/Lavandoo.4fc5c26b.png"
+  },
+  {
+    "revision": "ff36f843214c93bc139edfc9afab6577",
+    "url": "/static/media/Mtu.ff36f843.png"
+  },
+  {
+    "revision": "739804fb58bbe2bed205a8ab7d01ba8f",
+    "url": "/static/media/Socialmaps.739804fb.png"
+  },
+  {
+    "revision": "4d567253dea8caa5aa0ca20e0cf0022b",
+    "url": "/static/media/a.4d567253.png"
+  },
+  {
+    "revision": "03f31ca1aaeb0bcc2331c5ef0653d385",
+    "url": "/static/media/cricmetric1.03f31ca1.jpg"
+  },
+  {
+    "revision": "211715b528e6930606e5b8cf0bba41f2",
+    "url": "/static/media/cricmetric2.211715b5.jpg"
+  },
+  {
+    "revision": "7f6db6f6345273f66844d6d982d52fcd",
+    "url": "/static/media/cricmetric3.7f6db6f6.jpg"
+  },
+  {
+    "revision": "91ea05ffdc23c470b867fb21f3ddcc1e",
+    "url": "/static/media/cricmetric4.91ea05ff.jpg"
+  },
+  {
+    "revision": "03f31ca1aaeb0bcc2331c5ef0653d385",
+    "url": "/static/media/cricmetricShot.03f31ca1.jpg"
+  },
+  {
+    "revision": "e85de72f3e3efe4dc58cd37c7cda057e",
+    "url": "/static/media/icon.e85de72f.ico"
+  },
+  {
+    "revision": "27172678d2263dc84870ec09d2caf8c0",
+    "url": "/static/media/lavandoo1.27172678.jpg"
+  },
+  {
+    "revision": "52af4d67af9d44e393b113e7777563c2",
+    "url": "/static/media/lavandoo2.52af4d67.jpg"
+  },
+  {
+    "revision": "be243cebb585e01b5b9c3a5a72e24e1d",
+    "url": "/static/media/lavandoo3.be243ceb.jpg"
+  },
+  {
+    "revision": "0fe1b7c81ba0b534076349ee8ea4cb36",
+    "url": "/static/media/lavandoo4.0fe1b7c8.jpg"
+  },
+  {
+    "revision": "27172678d2263dc84870ec09d2caf8c0",
+    "url": "/static/media/lavandooShot.27172678.jpg"
+  },
+  {
+    "revision": "75ae2ba25d3d06d5079a940b79114940",
+    "url": "/static/media/loader.75ae2ba2.gif"
+  },
+  {
+    "revision": "60f3d1bf3bbc5de1c4bef8227a941e1c",
+    "url": "/static/media/mtu1.60f3d1bf.png"
+  },
+  {
+    "revision": "13e97a12230fc80cb7fa87e8b9d3d707",
+    "url": "/static/media/mtu2.13e97a12.png"
+  },
+  {
+    "revision": "424772b8c051cdd54ef514a7130d9f22",
+    "url": "/static/media/mtu3.424772b8.png"
+  },
+  {
+    "revision": "60f3d1bf3bbc5de1c4bef8227a941e1c",
+    "url": "/static/media/mtuShot.60f3d1bf.jpg"
+  },
+  {
+    "revision": "aa90f36524a0ef346f32f35e3c65c2cc",
+    "url": "/static/media/socialmaps1.aa90f365.jpg"
+  },
+  {
+    "revision": "7be5ab524975bb42a31becc15480bd08",
+    "url": "/static/media/socialmaps2.7be5ab52.jpg"
+  },
+  {
+    "revision": "2cf14a00603a0bfc3b2a82818892757a",
+    "url": "/static/media/socialmaps3.2cf14a00.jpg"
+  },
+  {
+    "revision": "a35307c02e48608983ff8bd76b1450a4",
+    "url": "/static/media/socialmaps4.a35307c0.jpg"
+  },
+  {
+    "revision": "aa90f36524a0ef346f32f35e3c65c2cc",
+    "url": "/static/media/socialmapsShot.aa90f365.jpg"
+  },
+  {
+    "revision": "8751f5e38fec3970c0ee36c8fad91cb2",
+    "url": "/static/media/suntrust0.8751f5e3.jpg"
+  },
+  {
+    "revision": "8751f5e38fec3970c0ee36c8fad91cb2",
+    "url": "/static/media/suntrustShot.8751f5e3.jpg"
+  },
+  {
+    "revision": "db9771c76fb2bab2c9e5af55acd24e79",
+    "url": "/static/media/suntrustpages.db9771c7.mp4"
+  },
+  {
+    "revision": "31d079c1eb9d6986e4e4f28ccabafb0b",
+    "url": "/static/media/tnatlas0.31d079c1.png"
+  },
+  {
+    "revision": "4b6bb26e8f22e296c299ff7e42bd1110",
+    "url": "/static/media/tnatlas1.4b6bb26e.png"
+  },
+  {
+    "revision": "da8fdfab225af4661250321791f3bb7f",
+    "url": "/static/media/tnatlas2.da8fdfab.png"
+  },
+  {
+    "revision": "0700a0b73b6da0ff6264e3164aabb730",
+    "url": "/static/media/tnatlas3.0700a0b7.png"
+  },
+  {
+    "revision": "ac1c5a9ef9fabcd3430ca32bdee03a3f",
+    "url": "/static/media/tnatlas3_1.ac1c5a9e.png"
+  },
+  {
+    "revision": "d23bcd107e987e20fe6b914d5446f1c6",
+    "url": "/static/media/tnatlas4.d23bcd10.png"
+  },
+  {
+    "revision": "6178c9df32f43f0805ff7cd63c99c99f",
+    "url": "/static/media/tnatlas5.6178c9df.png"
+  },
+  {
+    "revision": "2cf68c3c75246eb1352e949badc798dc",
+    "url": "/static/media/tnatlas6.2cf68c3c.png"
+  },
+  {
+    "revision": "31d079c1eb9d6986e4e4f28ccabafb0b",
+    "url": "/static/media/tnatlasShot.31d079c1.jpg"
+  }
+]);
